5565658aaf781ef4a5fcbc1cb67361ba8edeb1f7
Cmendence <chris.mendence@gmail.com>
2023-04-22 17:10:58 -0400

    now shows actual project

--- crud2.js
+++ crud2.js
@@ -100,3 +100,12 @@ function editTask(id) {
       });
     });
   }
+
+  document.addEventListener('keypress', function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === 'Enter') {
+      // Trigger the button element with a click
+      document.getElementById('task-btn').click();
+      document.getElementById('task-input').focus();
+    }
+  });
